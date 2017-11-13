40eed0dfadb29a32b07121e8f4c75f88ba3dc7d3
Biciato <leandrobiciato58@gmail.com>
2017-11-13 05:45:43 -0200

    menudown
    menudown

--- app/assets/javascripts/loja.js
+++ app/assets/javascripts/loja.js
@@ -8,9 +8,13 @@ $(function () {
 	function mudaImg() {
 
 		if (counter == 5) {
-			$("1, .2, .3, .4").hide();
-			counter = 1;
-		} else {	
+			$('.1').show();
+			$('button[data="1"]').css('background', 'silver');
+			$('button[data="4"]').css('background', 'gold');
+			$(".2, .3, .4").hide();
+			counter = 2;
+		} else {
+			$('.1').hide();
 			$(".".concat(counter)).show();
 			$('button').css('background', 'gold');
 			$("button[data=\"".concat(counter, "\"]")).css('background', 'silver');
