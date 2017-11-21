72ece2121cab0ee308a32b6fbe7262f68c47bca0
Biciato <leandrobiciato58@gmail.com>
2017-11-20 23:26:21 -0200

    começando o show produtos

--- app/assets/javascripts/loja.js
+++ app/assets/javascripts/loja.js
@@ -6,7 +6,6 @@ $(function () {
 	setInterval(mudaImg, 5000);
 
 	function mudaImg() {
-
 		if (counter == 5) {
 			$('.1').show();
 			$('button[data="1"]').css('background', 'silver');
@@ -21,16 +20,12 @@ $(function () {
 			counter++;
 		}
 	}
-
 	$("button").click(function() {
-
 		$(".1, .2, .3, .4").hide();
 		var e = $(this).attr("data");
 		$(".".concat(e)).show();
 		$('button').css('background', 'goldenrod');
 		$(this).css('background', 'silver');
 		counter = e;
-
 	});
-
 });
