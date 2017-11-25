b0c7f7b44745d3c8e9771262393fcde8987c64b5
Biciato <leandrobiciato58@gmail.com>
2017-11-25 14:58:55 -0200

    Merge branch 'master' of https://github.com/Biciato/loja
    finishing produto

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171008181358) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carrinhos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,6 +54,14 @@ ActiveRecord::Schema.define(version: 20171008181358) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
